55aae94a7f16acb79a07f964900b15c5c9dec0fd
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-07 13:58:22 +0500

    update

--- server/app/Http/Controllers/Teacher/ScheduleController.php
+++ server/app/Http/Controllers/Teacher/ScheduleController.php
@@ -10,18 +10,6 @@ use App\Models\GroupScheduleClass;
 
 class ScheduleController extends Controller
 {
-    public function getWeeklySchedule(Request $request)
-    {
-        $teacher = $request->user();
-        $today = now();
-        $startOfWeek = $today->copy()->startOfWeek();
-        $endOfWeek = $today->copy()->endOfWeek();
-
-        $schedule = $teacher->schedule()->whereBetween('date', [$startOfWeek, $endOfWeek])->get();
-
-        return response()->json($schedule);
-    }
-
 
     public function list(Request $request)
     {
@@ -30,22 +18,48 @@ class ScheduleController extends Controller
             'end_date' => 'nullable|date_format:Y-m-d'
         ]);
 
-        $teacher = $request->user;
+        $teacher = $request->user();
 
         // Получение входных данных или установка значений по умолчанию
         $startDate = $request->input('start_date') ?: Carbon::now()->startOfWeek()->format('Y-m-d');
         $endDate = $request->input('end_date') ?: Carbon::now()->addMonth()->format('Y-m-d');
-        $teacherId = $teacher['id'];
+        $teacherId = $teacher->id;
 
         // Получение расписания преподавателя за указанный период
-        $schedule = GroupScheduleClass::whereHas('subject.teacherSubject', function ($query) use ($teacherId) {
+        $scheduleClasses = GroupScheduleClass::whereHas('subject.teacherSubject', function ($query) use ($teacherId) {
             $query->where('userTeacherId', $teacherId);
         })
         ->whereBetween('date', [$startDate, $endDate])
-        ->with(['group', 'subject.teacherSubject.teacher'])
+        ->with(['group', 'subject.teacherSubject.subject'])
         ->get();
+
+        // Форматирование расписания в нужный формат
+        $schedule = [];
+        $period = CarbonPeriod::create($startDate, $endDate);
+
+        foreach ($period as $date) {
+            $dailySchedule = $scheduleClasses->filter(function ($class) use ($date) {
+                return $class->date->format('Y-m-d') == $date->format('Y-m-d');
+            });
+
+            $formattedDailySchedule = $dailySchedule->map(function ($class) {
+                return [
+                    'subject' => $class->subject->teacherSubject->subject->name,
+                    'group' => $class->group->title,
+                    'subgroup' => $class->subgroup,
+                    'number' => $class->number,
+                ];
+            });
+
+            $schedule[] = [
+                'day_of_week' => $date->format('l'),
+                'date' => $date->format('Y-m-d'),
+                'classes' => $formattedDailySchedule->values()->all(),
+            ];
+        }
 
         return response()->json($schedule);
     }
+
 
 }
