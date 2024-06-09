a9dbed6f6f6cea119e796e92887e102dde827929
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-09 15:54:00 +0500

    update

--- server/database/migrations/2024_04_30_112930_create_group_schedule_class_replacement_rq_table.php
+++ server/database/migrations/2024_04_30_112930_create_group_schedule_class_replacement_rq_table.php
@@ -14,12 +14,11 @@ return new class extends Migration
         // Таблица хранит запросы на замену.
         Schema::create('group_schedule_class_replacement_rq', function (Blueprint $table) {
             $table->id();
-            // Новые данные. Данные по номеру занятия и дню расписания берется через groupScheduleClassId
-            $table->foreignId('userTeacherId')->nullable()->constrained('user_teachers')->onUpdate('cascade')->onDelete('set null')->default(null);
-            // Данные по замене
-            $table->foreignId('groupScheduleClassId')->constrained(table:'group_schedule_classes')->cascadeOnUpdate()->cascadeOnDelete(); // Какое занятие заменяем.
+            $table->foreignId('groupId')->constrained(table:'groups')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamp('date');
+            $table->foreignId('subjectId')->nullable()->constrained('group_subjects')->onUpdate('cascade')->onDelete('set null')->default(null);
             $table->string('subgroup', 1)->nullable(); // A группа  /  B группа  / null - общая пара.
-            $table->string('reason');  // Причина.
+            $table->integer('number');
             $table->timestamps();
         });
     }
