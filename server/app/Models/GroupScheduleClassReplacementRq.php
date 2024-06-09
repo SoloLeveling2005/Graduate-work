c35133556b04f890cb161877b0d98ef5769418eb
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-09 16:22:05 +0500

    update

--- server/app/Models/GroupScheduleClassReplacementRq.php
+++ server/app/Models/GroupScheduleClassReplacementRq.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GroupScheduleClassReplacementRequest extends Model
+class GroupScheduleClassReplacementRq extends Model
 {
     use HasFactory;
 
