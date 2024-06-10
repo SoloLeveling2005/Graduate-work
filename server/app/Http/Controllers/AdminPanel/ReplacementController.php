9038ab44d18e09ad4752afb4ffb55789ba3795eb
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-10 13:15:48 +0500

    update

--- server/app/Http/Controllers/AdminPanel/ReplacementController.php
+++ server/app/Http/Controllers/AdminPanel/ReplacementController.php
@@ -54,7 +54,7 @@ class ReplacementController extends Controller
         }
 
         // Получение запросов на замену за указанный период
-        $requests = GroupScheduleClassReplacementRq::with(['teacher', 'scheduleClass'])
+        $requests = GroupScheduleClassReplacementRq::with(['subject', 'group'])
             ->whereBetween('created_at', [$startDate, $endDate])
             ->get();
 
