588e5654ed52b50bca0d721c3d687ae4db57d57b
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-12 01:03:35 +0500

    update

--- server/app/Http/Controllers/Teacher/Auth/TeacherAuthController.php
+++ server/app/Http/Controllers/Teacher/Auth/TeacherAuthController.php
@@ -26,7 +26,7 @@ class TeacherAuthController extends Controller
             return response()->json(['error' => "Passwords don't match"], 400);
         }
 
-        dd(Hash::check($valid_data['password'], $teacher->password));
+        dd(Hash::check("popov12", $teacher->password));
 
         if (!$teacher || !Hash::check($valid_data['password'], $teacher->password)) {
             return response()->json(['error' => 'Invalid login or password'], 401);
