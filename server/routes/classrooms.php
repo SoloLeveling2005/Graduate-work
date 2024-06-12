110d76f4c88d9ab1fa4ee0fc71e3ae1298b527b3
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-13 03:54:08 +0500

    update

---
+++ server/routes/classrooms.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Student\Auth\StudentAuthController as StudentAuthController;
+
+use App\Http\Middleware\Student\StudentAuthMiddleware as StudentAuthMiddleware;
+use App\Http\Middleware\Teacher\TeacherAuthMiddleware as TeacherAuthMiddleware;
+
+
+
+
