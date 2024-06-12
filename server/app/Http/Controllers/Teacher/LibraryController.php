eb167ab0959e38cfc0ddd573a66554c76301f0b4
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-13 01:08:03 +0500

    update

--- server/app/Http/Controllers/Teacher/LibraryController.php
+++ server/app/Http/Controllers/Teacher/LibraryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
-class LibraryController extends Controller
+class LibraryControllers extends Controller
 {
     public function store(Request $request)
     {
