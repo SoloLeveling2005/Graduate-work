d3f89716a44da600a16f0c802c6694e6c0b64bf3
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-12 23:56:11 +0500

    update

--- server/app/Http/Controllers/Teacher/ScheduleController.php
+++ server/app/Http/Controllers/Teacher/ScheduleController.php
@@ -50,11 +50,11 @@ class ScheduleController extends Controller
 
             $scheduleClasses = GroupScheduleClass::with(['subject.teacherSubject.teacher' => function($query) use ($teacher) {
                 $query->where('id', $teacher['id']);
-            }])->where('dayWeek', $dayWeek)->get();
+            }, 'subject.teacherSubject.subject'])->where('dayWeek', $dayWeek)->get();
 
             // Разделение общих занятий на подгруппы и группировка по номеру
             $processedClasses = [];
-            dd($scheduleClasses);
+            // dd($scheduleClasses);
 
             foreach ($scheduleClasses as $class) {
                 if (is_null($class->subgroup)) {
