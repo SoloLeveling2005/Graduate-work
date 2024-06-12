8c9bd54d280999f6d192247cd98582b3e971776e
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-13 02:40:10 +0500

    update

--- server/app/Http/Controllers/Teacher/CalendarController.php
+++ server/app/Http/Controllers/Teacher/CalendarController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\CalendarEvent;
 use App\Models\Group;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CalendarController extends Controller
