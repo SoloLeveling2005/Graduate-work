00d04f773066b5982c0183ab8a01754863da1d2e
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-13 01:43:47 +0500

    update

--- server/app/Http/Controllers/Student/LibraryController.php
+++ server/app/Http/Controllers/Student/LibraryController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class LibraryController extends Controller
 {
-    //
+    public function index()
+    {
+        $books = Book::all()->groupBy('subject');
+        return response()->json($books);
+    }
+
+    public function search(Request $request)
+    {
+        $validated = $request->validate([
+            'search' => 'required|string|max:255'
+        ]);
+
+        $books = Book::query();
+
+        $books->orWhere('author', 'like', '%' . $validated['search'] . '%');
+        $books->orWhere('class', 'like', '%' . $validated['search'] . '%');
+        $books->orWhere('title', 'like', '%' . $validated['search'] . '%');
+
+        return response()->json($books->get());
+    }
 }
