97bbed360001da86614aeb6efa5a7dce5b029c12
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-13 00:25:31 +0500

    update

--- server/app/Http/Controllers/Teacher/ScheduleController.php
+++ server/app/Http/Controllers/Teacher/ScheduleController.php
@@ -57,7 +57,7 @@ class ScheduleController extends Controller
 
                 dd(GroupScheduleClass::with(['subject.teacherSubject.teacher' => function($query) use ($teacher) {
                 $query->where('id', $teacher['id']);
-            }, 'subject.teacherSubject.subject', 'subject.teacherSubject.teacher'])->where('dayWeek', 1)->get()->toArray());
+            }, 'subject.teacherSubject.subject'])->where('dayWeek', 1)->get()->toArray());
 
             foreach ($scheduleClasses as $class) {
                 if (is_null($class->subgroup)) {
@@ -92,7 +92,7 @@ class ScheduleController extends Controller
             'id' => $class->id,
             'groupId' => $class->groupId,
             'subjectId' => $class->subjectId,
-            'subject' => $class->subject->teacher_subject->subject->title,
+            'subject' => $class['subject']['teacher_subject']['subject']['title'],
             'subgroup' => $subgroup,
             'number' => $class->number,
             'dayWeek' => $class->dayWeek,
