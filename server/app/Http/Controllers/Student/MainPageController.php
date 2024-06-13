2457ad63504a741ca6a6d1b029c3271ab994bbdd
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-13 23:21:42 +0500

    update

--- server/app/Http/Controllers/Student/MainPageController.php
+++ server/app/Http/Controllers/Student/MainPageController.php
@@ -51,7 +51,7 @@ class MainPageController extends Controller
             return $item['number'] == $dayOfWeek;
         });
 
-        return response()->json($schedule, 200);
+        return response()->json($dayOfWeek, 200);
     }
 
     public function getTodayShedule(Request $request) {
