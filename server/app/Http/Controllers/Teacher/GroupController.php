32858ee77eb4a3a88dc9b79c693cfafc7eac36c4
SoloLeveling2005 <SoloLevelingMansur2005@gmail.com>
2024-06-14 02:32:59 +0500

    update

--- server/app/Http/Controllers/Teacher/GroupController.php
+++ server/app/Http/Controllers/Teacher/GroupController.php
@@ -43,14 +43,14 @@ class GroupController extends Controller
         $teacher = $request->user;
         $teacherId = $teacher['id'];
 
-        $group = Group::with(['subjects.teacherSubject.teacher','students','schedules.subject.teacherSubject.teacher.auditorium'])->find($groupId);
+        $group = Group::with(['subjects.teacherSubject.teacher','subjects.teacherSubject.subject', 'students','schedules.subject.teacherSubject.teacher.auditorium'])->find($groupId);
 
         if (!$group) {
             return response()->json(['error' => '404 Group Not Found', 'status'=>404], 404);
         }
 
         // TODO - Какие предметы ведет в группе этот препод
-        // TODO - Какое расписание на неделю у этого препода в этой группе
+        // TODO - Какое расписание на неделю у этого препода в этой группе  (день недели, номер пары, название предмета, аудитория)
         // TODO - Список группы
 
         $subjects = ($group->subjects)->filter(function($item) use ($teacherId) {
@@ -58,6 +58,19 @@ class GroupController extends Controller
         });
         $schedules = $group->schedules->filter(function($item) use ($teacherId) {
             return $item->subject->teacherSubject->userTeacherId == $teacherId;
+        })->map(function($item) {
+            return [
+                "id"=> $item->id,
+                "groupId"=> $item->groupId,
+                "subjectId"=> $item->subjectId,
+                "subgroup"=> $item->subgroup,
+                "number"=> $item->number,
+                "dayWeek"=> $item->dayWeek,
+                "created_at"=> $item->created_at,
+                "updated_at"=> $item->updated_at,
+                "subject"=> $item->subject->teacher_subject->subject->title,
+                "auditorium"=> $item->subject->teacher_subject->teacher->auditorium->number
+            ]
         })->groupBy('dayWeek');
         $students = $group->students;
 
